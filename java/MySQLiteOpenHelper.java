06195b3f87854576c30041c185ff919dda511581
Ellieka <42927706+tmdgpdhkdehd@users.noreply.github.com>
2021-12-02 19:20:27 +0900

    Update MySQLiteOpenHelper.java

--- java/MySQLiteOpenHelper.java
+++ java/MySQLiteOpenHelper.java
@@ -29,7 +29,6 @@ import android.database.sqlite.SQLiteOpenHelper;
          * 위의 세가지 기능을 사용해야한다.
          */
 
-
 public class MySQLiteOpenHelper extends SQLiteOpenHelper {
 
     public MySQLiteOpenHelper(Context context, String name,
@@ -38,36 +37,26 @@ public class MySQLiteOpenHelper extends SQLiteOpenHelper {
         // TODO Auto-generated constructor stub
     }
 
+
     @Override
     public void onCreate(SQLiteDatabase db) {
 
         // TODO Auto-generated method stub
-        // SQL 쿼리문은 다음과 같은 형태로도 실행 할 수도 있다.
-        // SQLiteOpenHelper 가 최초 실행 되었을 때
         // 최초 db 생성 작업
+
         String sql = "create table building" +
-                "(name text, " +
-                "ingredient text, " +
-                "additive text);";
+                "(id integer primary key autoincrement, " +
+                "name text, " +
+                "address text);";
         db.execSQL(sql);
     }
+
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
-        // db = 적용할 db, old/new 구 버전/신버전
         // TODO Auto-generated method stub
-        /*
-         * db 버전이 업그레이드 되었을 때 실행되는 메소드
-         * 이 부분은 사용에 조심해야 하는 일이 많이 있다. 버전이 1인 사용자가 2로 바뀌면
-         * 한번의 수정만 하면 되지만 버전이 3으로 되면 1인 사용자가 2, 3을 거쳐야 하고
-         * 2인 사용자는 3 까지만 거치면 된다. 이렇게 증가할 수록 수정할 일이 많아지므로
-         * 적절히 사용해야 하며 가능하면 최초 설계 시에 완벽을 기하는 것이 가장 좋을 것이다.
-         * 테스트에서는 기존의 데이터를 모두 지우고 다시 만드는 형태로 하겠다.
-         */
-
-        String sql = "drop table if exists student";
-        db.execSQL(sql);
-        onCreate(db); // 테이블을 지웠으므로 다시 테이블을 만들어주는 과정
+        String sql = "drop table if exists building;";
+        onCreate(db); // table 다시 만들기
     }
 }
