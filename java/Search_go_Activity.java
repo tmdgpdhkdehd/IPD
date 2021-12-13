6aaa618bc7c068ee9482cdb027d87d889b04ccd8
Ellieka <42927706+tmdgpdhkdehd@users.noreply.github.com>
2021-12-13 20:41:10 +0900

    Update Search_go_Activity.java

--- java/Search_go_Activity.java
+++ java/Search_go_Activity.java
@@ -1,59 +1,81 @@
 package com.example.ipd;
 
+
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.content.res.ColorStateList;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
-import android.graphics.drawable.BitmapDrawable;
-import android.media.ExifInterface;
-import android.net.Uri;
-import android.os.Build;
+import android.media.ThumbnailUtils;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.content.FileProvider;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
-import android.view.ViewManager;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.Toast;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
+import android.widget.TextView;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ipd.ml.Model;
+
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 public class Search_go_Activity extends AppCompatActivity {
 
-    private static final String TAG = "CameraActivity";
-    public static final int REQUEST_TAKE_PHOTO = 10;
-    public static final int REQUEST_PERMISSION = 11;
-    private Button btnCamera, btnSave;
-    private ImageView ivCapture;
-    private String mCurrentPhotoPath;
+    TextView result, confidence;
+    ImageView imageView;
+    int imageSize = 224;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_go);
 
-        checkPermission(); //권한체크
-        ivCapture = findViewById(R.id.ivCapture); //ImageView 선언
-        btnCamera = findViewById(R.id.btnCapture); //Button 선언
-        btnSave = findViewById(R.id.btnSave); //Button 선언
+        result = findViewById(R.id.result);
+        confidence = findViewById(R.id.confidence);
+        imageView = findViewById(R.id.imageView);
         Button back_btn = (Button) findViewById(R.id.back_btn);
+        Button uusa_btn = (Button) findViewById(R.id.uusa_btn);
+        Button gold_btn = (Button) findViewById(R.id.gold_btn);
+        Button vitamin_btn = (Button) findViewById(R.id.vitamin_btn);
+        Button tylenol_btn = (Button) findViewById(R.id.tylenol_btn);
+
+        uusa_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View uusa_btn) {
+                Intent intent = new Intent(getApplicationContext(), Uusa_Activity.class);
+                startActivityForResult(intent,1);
+            }
+        });
+
+        gold_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View gold_btn) {
+                Intent intent = new Intent(getApplicationContext(), Gold_Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        vitamin_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View vitamin_btn) {
+                Intent intent = new Intent(getApplicationContext(), Vitamin_Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        tylenol_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View tylenol_btn) {
+                Intent intent = new Intent(getApplicationContext(), Tylenol_Activity.class);
+                startActivity(intent);
+            }
+        });
 
         back_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,206 +84,86 @@ public class Search_go_Activity extends AppCompatActivity {
             }
         });
 
-        loadImgArr();
-        //촬영
-        btnCamera.setOnClickListener(v -> captureCamera());
-        //저장
-        btnSave.setOnClickListener(v -> {
-            try {
-                BitmapDrawable drawable = (BitmapDrawable) ivCapture.getDrawable();
-                Bitmap bitmap = drawable.getBitmap();
-                //찍은 사진이 없으면
-                if (bitmap == null) {
-                    Toast.makeText(this, "저장할 사진이 없습니다.", Toast.LENGTH_SHORT).show();
-                } else {
-                    //저장
-                    saveImg();
-                    mCurrentPhotoPath = ""; //initialize
-                }
-
-            } catch (Exception e) {
-                Log.w(TAG, "SAVE ERROR!", e);
-            }
-        });
-    }
-
-    private void captureCamera() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // 인텐트를 처리 할 카메라 액티비티가 있는지 확인
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            // 촬영한 사진을 저장할 파일 생성
-            File photoFile = null;
-            try {
-                //임시로 사용할 파일이므로 경로는 캐시폴더로
-                File tempDir = getCacheDir();
-                //임시촬영파일 세팅
-                String timeStamp = new SimpleDateFormat("yyyyMMdd").format(new Date());
-                String imageFileName = "Capture_" + timeStamp + "_"; //ex) Capture_20201206_
-                File tempImage = File.createTempFile(
-                        imageFileName, /* 파일이름 */
-                        ".jpg", /* 파일형식 */
-                        tempDir /* 경로 */
-                );
-                // ACTION_VIEW 인텐트를 사용할 경로 (임시파일의 경로)
-                mCurrentPhotoPath = tempImage.getAbsolutePath();
-                photoFile = tempImage;
-            } catch (IOException e) {
-                //에러 로그는 이렇게 관리하는 편이 좋다.
-                Log.w(TAG, "파일 생성 에러!", e);
-            }
-            //파일이 정상적으로 생성되었다면 계속 진행
-            if (photoFile != null) {
-                //Uri 가져오기
-                Uri photoURI = FileProvider.getUriForFile(this,
-                        getPackageName() + ".fileprovider",
-                        photoFile);
-                //인텐트에 Uri담기
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                //인텐트 실행
-                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
-            }
+        if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(cameraIntent, 1);
+        } else {
+            //Request camera permission if we don't have it.
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, 100);
         }
     }
 
-    //이미지저장 메소드
-    private void saveImg() {
+    public void classifyImage(Bitmap image) {
         try {
-            //저장할 파일 경로
-            File storageDir = new File(getFilesDir() + "/capture");
-            if (!storageDir.exists()) //폴더가 없으면 생성.
-                storageDir.mkdirs();
-            String filename = "캡쳐파일" + ".jpg";
-            // 기존에 있다면 삭제
-            File file = new File(storageDir, filename);
-            boolean deleted = file.delete();
-            Log.w(TAG, "Delete Dup Check : " + deleted);
-            FileOutputStream output = null;
-            try {
-                output = new FileOutputStream(file);
-                BitmapDrawable drawable = (BitmapDrawable) ivCapture.getDrawable();
-                Bitmap bitmap = drawable.getBitmap();
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 70, output); //해상도에 맞추어 Compress
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } finally {
-                try {
-                    assert output != null;
-                    output.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
+            Model model = Model.newInstance(getApplicationContext());
+
+            // Creates inputs for reference.
+            TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.FLOAT32);
+            ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3);
+            byteBuffer.order(ByteOrder.nativeOrder());
+
+            // get 1D array of 224 * 224 pixels in image
+            int[] intValues = new int[imageSize * imageSize];
+            image.getPixels(intValues, 0, image.getWidth(), 0, 0, image.getWidth(), image.getHeight());
+
+            // iterate over pixels and extract R, G, and B values. Add to bytebuffer.
+            int pixel = 0;
+            for (int i = 0; i < imageSize; i++) {
+                for (int j = 0; j < imageSize; j++) {
+                    int val = intValues[pixel++]; // RGB
+                    byteBuffer.putFloat(((val >> 16) & 0xFF) * (1.f / 255.f));
+                    byteBuffer.putFloat(((val >> 8) & 0xFF) * (1.f / 255.f));
+                    byteBuffer.putFloat((val & 0xFF) * (1.f / 255.f));
                 }
             }
-            Log.e(TAG, "Captured Saved");
-            Toast.makeText(this, "Capture Saved ", Toast.LENGTH_SHORT).show();
-        } catch (Exception e) {
-            Log.w(TAG, "Capture Saving Error!", e);
-            Toast.makeText(this, "Save failed", Toast.LENGTH_SHORT).show();
+
+            inputFeature0.loadBuffer(byteBuffer);
+
+            // Runs model inference and gets result.
+            Model.Outputs outputs = model.process(inputFeature0);
+            TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+
+            float[] confidences = outputFeature0.getFloatArray();
+            // find the index of the class with the biggest confidence.
+            int maxPos = 0;
+            float maxConfidence = 0;
+            for (int i = 0; i < confidences.length; i++) {
+                if (confidences[i] > maxConfidence) {
+                    maxConfidence = confidences[i];
+                    maxPos = i;
+                }
+            }
+            String[] classes = {"Uusa", "B-max Gold", "Tylenol", "VitaminC 1000"};
+            result.setText(classes[maxPos]);
+
+            String s = "";
+            for (int i = 0; i < classes.length; i++) {
+
+                    s += String.format("%s: %.1f%%\n", classes[i], confidences[i] * 100);
+            }
+
+
+            confidence.setText(s);
+            // Releases model resources if no longer used.
+            model.close();
+        } catch (IOException e) {
+            // TODO Handle the exception
         }
     }
 
-    private void loadImgArr() {
-        try {
-            File storageDir = new File(getFilesDir() + "/capture");
-            String filename = "캡쳐파일" + ".jpg";
-            File file = new File(storageDir, filename);
-            Bitmap bitmap = BitmapFactory.decodeStream(new FileInputStream(file));
-            ivCapture.setImageBitmap(bitmap);
-        } catch (Exception e) {
-            Log.w(TAG, "Capture loading Error!", e);
-            Toast.makeText(this, "load failed", Toast.LENGTH_SHORT).show();
-        }
-    }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        super.onActivityResult(requestCode, resultCode, intent);
-        try {
-            //after capture
-            switch (requestCode) {
-                case REQUEST_TAKE_PHOTO: {
-                    if (resultCode == RESULT_OK) {
-                        File file = new File(mCurrentPhotoPath);
-                        Bitmap bitmap = MediaStore.Images.Media
-                                .getBitmap(getContentResolver(), Uri.fromFile(file));
-                        if (bitmap != null) {
-                            ExifInterface ei = new ExifInterface(mCurrentPhotoPath);
-                            int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION,
-                                    ExifInterface.ORIENTATION_UNDEFINED);
-// //사진해상도가 너무 높으면 비트맵으로 로딩
-// BitmapFactory.Options options = new BitmapFactory.Options();
-// options.inSampleSize = 8; //8분의 1크기로 비트맵 객체 생성
-// Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath(), options);
-                            Bitmap rotatedBitmap = null;
-                            switch (orientation) {
-                                case ExifInterface.ORIENTATION_ROTATE_90:
-                                    rotatedBitmap = rotateImage(bitmap, 90);
-                                    break;
-                                case ExifInterface.ORIENTATION_ROTATE_180:
-                                    rotatedBitmap = rotateImage(bitmap, 180);
-                                    break;
-                                case ExifInterface.ORIENTATION_ROTATE_270:
-                                    rotatedBitmap = rotateImage(bitmap, 270);
-                                    break;
-                                case ExifInterface.ORIENTATION_NORMAL:
-                                default:
-                                    rotatedBitmap = bitmap;
-                            }
-                            //Rotate한 bitmap을 ImageView에 저장
-                            ivCapture.setImageBitmap(rotatedBitmap);
-                        }
-                    }
-                    break;
-                }
-            }
-        } catch (Exception e) {
-            Log.w(TAG, "onActivityResult Error !", e);
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            Bitmap image = (Bitmap) data.getExtras().get("data");
+            int dimension = Math.min(image.getWidth(), image.getHeight());
+            image = ThumbnailUtils.extractThumbnail(image, dimension, dimension);
+            imageView.setImageBitmap(image);
+
+            image = Bitmap.createScaledBitmap(image, imageSize, imageSize, false);
+            classifyImage(image);
         }
-    }
-
-    //카메라에 맞게 이미지 로테이션
-    public static Bitmap rotateImage(Bitmap source, float angle) {
-        Matrix matrix = new Matrix();
-        matrix.postRotate(angle);
-        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(),
-                matrix, true);
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        checkPermission(); //권한체크
-    }
-
-    //권한 확인
-    public void checkPermission() {
-        int permissionCamera = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
-        int permissionRead = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
-        int permissionWrite = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        //권한이 없으면 권한 요청
-        if (permissionCamera != PackageManager.PERMISSION_GRANTED
-                || permissionRead != PackageManager.PERMISSION_GRANTED
-                || permissionWrite != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
-                Toast.makeText(this, "이 앱을 실행하기 위해 권한이 필요합니다.", Toast.LENGTH_SHORT).show();
-            }
-            ActivityCompat.requestPermissions(this, new String[]{
-                    Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_PERMISSION);
-
-        }
-
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case REQUEST_PERMISSION: {
-                // 권한이 취소되면 result 배열은 비어있다.
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                } else {
-                    finish(); //권한이 없으면 앱 종료
-                }
-            }
-        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
