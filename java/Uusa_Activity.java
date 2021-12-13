64b858a89506021eabd7af02b2f1552d0bf8306a
Ellieka <42927706+tmdgpdhkdehd@users.noreply.github.com>
2021-12-13 20:42:19 +0900

    Create Uusa_Activity.java

---
+++ java/Uusa_Activity.java
@@ -0,0 +1,32 @@
+package com.example.ipd;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class Uusa_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uusa);
+
+        Button back_btn = (Button) findViewById(R.id.back_btn);
+
+        back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View search_go_btn) {
+                onBackPressed();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+}
