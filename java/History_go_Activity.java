b1bcb633130005b9d5ab0bb53c9756f25acaf08d
Ellieka <42927706+tmdgpdhkdehd@users.noreply.github.com>
2021-12-27 16:29:49 +0900

    Update History_go_Activity.java

--- java/History_go_Activity.java
+++ java/History_go_Activity.java
@@ -1,53 +1,25 @@
 package com.example.ipd;
 
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
-import android.os.Bundle;
-import android.util.Base64;
-import android.util.Log;
 import android.app.Activity;
-import android.content.ContentValues;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.os.Handler;
+import android.content.Intent;
+import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.MotionEvent;
 import android.view.View;
-import android.widget.AutoCompleteTextView;
+import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
-import java.util.concurrent.ScheduledExecutorService;
 
 public class History_go_Activity extends Activity {
+    List<String> list;
     ListView listView;
     EditText editSearch;        // 검색어를 입력할 Input 창
-    SearchAdapter adapter;
     ArrayList<String> arraylist;
-    List<String> list;
-    ArrayList<MyBuilding> al = new ArrayList<MyBuilding>();
+    SearchAdapter adapter;
+
 
     @Override
     protected void onResume() {
@@ -56,142 +28,66 @@ public class History_go_Activity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history_go);
+        editSearch = findViewById(R.id.history_EditText);
+        listView = this.findViewById(R.id.history_listView);
 
-//        clearApplicationData();
+        list = new ArrayList<>();
 
-        Button back_btn = (Button) findViewById(R.id.back_btn);
+        list.add("Uusa");
+        list.add("VitaminC 1000");
+        list.add("B-Max Gold");
+        list.add("Tylenol");
 
-        back_btn.setOnClickListener(new View.OnClickListener() {
+        arraylist = new ArrayList<>();
+        arraylist.addAll(list);
+
+
+        adapter = new SearchAdapter(list, this);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
             @Override
-            public void onClick(View search_go_btn) {
-                onBackPressed();
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                if(position == 0){
+                    Intent intent = new Intent(getApplicationContext(), Uusa_Activity.class);
+                    startActivity(intent);
+                } else if(position == 1){
+                    Intent intent = new Intent(getApplicationContext(), Vitamin_Activity.class);
+                    startActivity(intent);
+                } else if(position == 2){
+                    Intent intent = new Intent(getApplicationContext(), Gold_Activity.class);
+                    startActivity(intent);
+                } else if(position == 3){
+                    Intent intent = new Intent(getApplicationContext(), Tylenol_Activity.class);
+                    startActivity(intent);
+                }
+
             }
         });
 
-        editSearch = (EditText) findViewById(R.id.edittext);
-        listView = (ListView)this.findViewById(R.id.listView1);
-
-        // 리스트를 생성한다.
-        list = new ArrayList<String>();
-
-        // 검색에 사용할 데이터을 미리 저장한다.
-        settingList();
-//        ArrayList<String> items = new ArrayList<>();
-//        items.add("Tylenol");
-//        items.add("Gold");
-
-        // 리스트의 모든 데이터를 arraylist에 복사한다.// list 복사본을 만든다.
-        arraylist = new ArrayList<String>();
-        arraylist.addAll(list);
-
-        // 리스트에 연동될 아답터를 생성한다.
-        adapter = new SearchAdapter(list, this);
-
-        // 리스트뷰에 아답터를 연결한다.
-//        CustomAdapter adapter = new CustomAdapter(this, 0, items);
-        listView.setAdapter(adapter);
-
-        // input창에 검색어를 입력시 "addTextChangedListener" 이벤트 리스너를 정의한다.
         editSearch.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
             }
 
             @Override
-            public void afterTextChanged(Editable editable) {
-                // input창에 문자를 입력할때마다 호출된다.
-                // search 메소드를 호출한다.
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
                 String text = editSearch.getText().toString();
                 search(text);
             }
         });
     }
 
-    private class CustomAdapter extends ArrayAdapter<String> {
-        private ArrayList<String> items;
 
-        public CustomAdapter(Context context, int textViewResourceId, ArrayList<String> objects) {
-            super(context, textViewResourceId, objects);
-            this.items = objects;
-        }
-        public View getView(int position, View convertView, ViewGroup parent) {
-            View v = convertView;
-            if (v == null) {
-                LayoutInflater vi = (LayoutInflater) getSystemService
-                        (Context.LAYOUT_INFLATER_SERVICE);
-                v = vi.inflate(R.layout.row, null);
-            }
-// ImageView 인스턴스
-            ImageView imageView = (ImageView)v.findViewById(R.id.imageView);
-// 리스트뷰의 아이템에 이미지를 변경한다.
-            if("Uusa".equals(items.get(position)))
-                imageView.setImageResource(R.drawable.uusa_image);
-            else if("B-max Gold".equals(items.get(position)))
-                imageView.setImageResource(R.drawable.gold_image);
-            else if("Tylenol".equals(items.get(position)))
-                imageView.setImageResource(R.drawable.tylenol_image);
-            else if("VitaminC 1000".equals(items.get(position)))
-                imageView.setImageResource(R.drawable.vitamin_image);
-            TextView textView = (TextView)v.findViewById(R.id.textView1);
-            textView.setText(items.get(position));
-            final String text = items.get(position);
-            Button button = (Button)v.findViewById(R.id.button);
-            button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Toast.makeText(History_go_Activity.this, text, Toast.LENGTH_SHORT).show();
-                }
-            });
-            return v;
-        }
-    }
-
-
-    // 데이터 삭제
-    public void clearApplicationData() {
-        File cache = getCacheDir();
-        try {
-        } catch (Exception e) {
-        }
-        File appDir = new File(cache.getParent());
-        if (appDir.exists()) {
-            String[] children = appDir.list();
-            for (String s : children) {
-                if (!s.equals("lib") && !(s.equals("shared_prefs"))) {
-                    deleteDir(new File(appDir, s));
-                }
-            }
-        }
-    }
-
-    public static boolean deleteDir(File dir) {
-        if (dir != null && dir.isDirectory()) {
-            String[] children = dir.list();
-            for (int i = 0; i < children.length; i++) {
-                boolean success = deleteDir(new File(dir, children[i]));
-                if (!success) {
-                    return false;
-                }
-            }
-        }
-        // The directory is now empty or this is a file so delete it
-        return dir.delete();
-    }
-
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-    }
-
-    // 검색을 수행하는 메소드
     public void search(String charText) {
 
         // 문자 입력시마다 리스트를 지우고 새로 뿌려준다.
@@ -207,7 +103,7 @@ public class History_go_Activity extends Activity {
             // 리스트의 모든 데이터를 검색한다.
             for(int i = 0;i < arraylist.size(); i++)
             {
-                // arraylist의 모든 데이터에 입력받은 단어(charText)가 포함되어 있으면 true를 반환한다.
+                // arraylist 의 모든 데이터에 입력받은 단어(charText)가 포함되어 있으면 true 를 반환한다.
                 if (arraylist.get(i).toLowerCase().contains(charText))
                 {
                     // 검색된 데이터를 리스트에 추가한다.
@@ -218,12 +114,4 @@ public class History_go_Activity extends Activity {
         // 리스트 데이터가 변경되었으므로 아답터를 갱신하여 검색된 데이터를 화면에 보여준다.
         adapter.notifyDataSetChanged();
     }
-
-    // 검색에 사용될 데이터를 리스트에 추가한다.
-    private void settingList(){
-        list.add("Uusa");
-        list.add("B-max Gold");
-        list.add("Tylenol");
-        list.add("VitaminC 1000");
-    }
-} // end class MainActivity
+}
